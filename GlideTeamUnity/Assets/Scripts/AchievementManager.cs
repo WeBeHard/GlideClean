5229914ec348574c6e2e29ca611fb03b0e17100f
Andrew Vasquez <vasquezandrew93@gmail.com>
2018-05-03 15:22:08 -0400

    Achievement Fix

--- GlideTeamUnity/Assets/Scripts/AchievementManager.cs
+++ GlideTeamUnity/Assets/Scripts/AchievementManager.cs
@@ -21,29 +21,29 @@ public class AchievementManager : MonoBehaviour
 
     public void clearAllAchievements()
     {
-        PlayerPrefs.SetInt("AchievementGAMEOVER") = 0;
-        PlayerPrefs.SetInt("AchievementTEAMTEN") = 0;
-        PlayerPrefs.SetInt("AchievementPLAYERCENTURY") = 0;
-        PlayerPrefs.SetInt("AchievementGRANDMASTER") = 0;
-        PlayerPrefs.SetInt("AchievementPARTICIPANT") = 0;
-        PlayerPrefs.SetInt("AchievementPERSISTENT") = 0;
-        PlayerPrefs.SetInt("AchievementUNYIELDING") = 0;
-        PlayerPrefs.SetInt("AchievementSTILLHERE") = 0;
-        PlayerPrefs.SetInt("AchievementTHECLASSICS") = 0;
-        PlayerPrefs.SetInt("AchievementSPEEDDEMON") = 0;
-        PlayerPrefs.SetInt("AchievementNORULES") = 0;
-        PlayerPrefs.SetInt("AchievementACHIEVEMENTHUNTER") = 0;
+        PlayerPrefs.SetInt("AchievementGAMEOVER", 0);
+        PlayerPrefs.SetInt("AchievementTEAMTEN", 0);
+        PlayerPrefs.SetInt("AchievementPLAYERCENTURY", 0);
+        PlayerPrefs.SetInt("AchievementGRANDMASTER", 0);
+        PlayerPrefs.SetInt("AchievementPARTICIPANT", 0);
+        PlayerPrefs.SetInt("AchievementPERSISTENT", 0);
+        PlayerPrefs.SetInt("AchievementUNYIELDING", 0);
+        PlayerPrefs.SetInt("AchievementSTILLHERE", 0);
+        PlayerPrefs.SetInt("AchievementTHECLASSICS", 0);
+        PlayerPrefs.SetInt("AchievementSPEEDDEMON", 0);
+        PlayerPrefs.SetInt("AchievementNORULES", 0);
+        PlayerPrefs.SetInt("AchievementACHIEVEMENTHUNTER", 0);
     }
 
     public void checkAchievementGAMEOVER()
     {
         if (PlayerPrefs.GetInt("TotalGamesPlayed") >= 1)
         {
-          PlayerPrefs.SetInt("AchievementGAMEOVER")  = 1;
+			PlayerPrefs.SetInt("AchievementGAMEOVER", 1);
         }
         else
         {
-            PlayerPrefs.SetInt("AchievementGAMEOVER") = 0;
+			PlayerPrefs.SetInt("AchievementGAMEOVER", 0);
         }
     }
 
@@ -51,11 +51,11 @@ public class AchievementManager : MonoBehaviour
     {
         if (PlayerPrefs.GetInt("TotalGamesPlayed") >= 10)
         {
-            PlayerPrefs.SetInt("AchievementTEAMTEN") = 1;
+			PlayerPrefs.SetInt("AchievementTEAMTEN", 1);
         }
         else
         {
-            PlayerPrefs.SetInt("AchievementTEAMTEN") = 0;
+			PlayerPrefs.SetInt ("AchievementTEAMTEN", 0);
         }
     }
 
@@ -63,11 +63,11 @@ public class AchievementManager : MonoBehaviour
     {
         if (PlayerPrefs.GetInt("TotalGamesPlayed") >= 100)
         {
-            PlayerPrefs.SetInt("AchievementPLAYERCENTURY") = 1;
+			PlayerPrefs.SetInt("AchievementPLAYERCENTURY", 1);
         }
         else
         {
-            PlayerPrefs.SetInt("AchievementPLAYERCENTURY") = 0;
+			PlayerPrefs.SetInt("AchievementPLAYERCENTURY", 0);
         }
     }
 
@@ -75,11 +75,11 @@ public class AchievementManager : MonoBehaviour
     {
         if (PlayerPrefs.GetInt("TotalGamesPlayed") >= 1000)
         {
-            PlayerPrefs.SetInt("AchievementGRANDMASTER") = 1;
+			PlayerPrefs.SetInt("AchievementGRANDMASTER", 1);
         }
         else
         {
-            PlayerPrefs.SetInt("AchievementGRANDMASTER") = 0;
+			PlayerPrefs.SetInt("AchievementGRANDMASTER", 0);
         }
     }
 
@@ -87,11 +87,11 @@ public class AchievementManager : MonoBehaviour
     {
         if (PlayerPrefs.GetInt("CumulativeScore") >= 1000)
         {
-            PlayerPrefs.SetInt("AchievementPARTICIPANT") = 1;
+			PlayerPrefs.SetInt("AchievementPARTICIPANT", 1);
         }
         else
         {
-            PlayerPrefs.SetInt("AchievementPARTICIPANT") = 0;
+			PlayerPrefs.SetInt("AchievementPARTICIPANT", 0);
         }
     }
 
@@ -99,11 +99,11 @@ public class AchievementManager : MonoBehaviour
     {
         if (PlayerPrefs.GetInt("CumulativeScore") >= 10000)
         {
-            PlayerPrefs.SetInt("AchievementPERSISTENT") = 1;
+			PlayerPrefs.SetInt("AchievementPERSISTENT", 1);
         }
         else
         {
-            PlayerPrefs.SetInt("AchievementPERSISTENT") = 0;
+			PlayerPrefs.SetInt("AchievementPERSISTENT", 0);
         }
     }
 
@@ -111,11 +111,11 @@ public class AchievementManager : MonoBehaviour
     {
         if (PlayerPrefs.GetInt("CumulativeScore") >= 100000)
         {
-            PlayerPrefs.SetInt("AchievementUNYIELDING") = 1;
+			PlayerPrefs.SetInt("AchievementUNYIELDING", 1);
         }
         else
         {
-            PlayerPrefs.SetInt("AchievementUNYIELDING") = 0;
+			PlayerPrefs.SetInt ("AchievementUNYIELDING", 0);
         }
     }
 
@@ -123,11 +123,11 @@ public class AchievementManager : MonoBehaviour
     {
         if (PlayerPrefs.GetInt("CumulativeScore") >= 1000000)
         {
-            PlayerPrefs.SetInt("AchievementSTILLHERE") = 1;
+			PlayerPrefs.SetInt("AchievementSTILLHERE", 0);
         }
         else
         {
-            PlayerPrefs.SetInt("AchievementSTILLHERE") = 0;
+			PlayerPrefs.SetInt("AchievementSTILLHERE", 0);
         }
     }
 
@@ -135,11 +135,11 @@ public class AchievementManager : MonoBehaviour
     {
         if (PlayerPrefs.GetInt("ClassicTotalGamesPlayed") >= 100)
         {
-            PlayerPrefs.SetInt("AchievementTHECLASSICS") = 1;
+			PlayerPrefs.SetInt("AchievementTHECLASSICS", 1);
         }
         else
         {
-            PlayerPrefs.SetInt("AchievementTHECLASSICS") = 0;
+			PlayerPrefs.SetInt("AchievementTHECLASSICS", 0);
         }
     }
 
@@ -147,11 +147,11 @@ public class AchievementManager : MonoBehaviour
     {
         if (PlayerPrefs.GetInt("TimeAttackTotalGamesPlayed") >= 100)
         {
-            PlayerPrefs.SetInt("AchievementSPEEDDEMON") = 1;
+			PlayerPrefs.SetInt("AchievementSPEEDDEMON", 1);
         }
         else
         {
-            PlayerPrefs.SetInt("AchievementSPEEDDEMON") = 0;
+			PlayerPrefs.SetInt("AchievementSPEEDDEMON", 0);
         }
     }
 
@@ -159,33 +159,33 @@ public class AchievementManager : MonoBehaviour
     {
         if (PlayerPrefs.GetInt("PowersTotalGamesPlayed") >= 100)
         {
-            PlayerPrefs.SetInt("AchievementNORULES") = 1;
+			PlayerPrefs.SetInt("AchievementNORULES", 1);
         }
         else
         {
-            PlayerPrefs.SetInt("AchievementNORULES") = 0;
+			PlayerPrefs.SetInt("AchievementNORULES", 0);
         }
     }
 
     public void checkAchievementACHIEVEMENTHUNTER()
     {
-        if (PlayerPrefs.GetInt("AchievementGAMEOVER") = 1 &&
-        PlayerPrefs.GetInt("AchievementTEAMTEN") = 1 &&
-        PlayerPrefs.GetInt("AchievementPLAYERCENTURY") = 1 &&
-        PlayerPrefs.GetInt("AchievementGRANDMASTER") = 1 && 
-        PlayerPrefs.GetInt("AchievementPARTICIPANT") = 1 && 
-        PlayerPrefs.GetInt("AchievementPERSISTENT") = 1 &&
-        PlayerPrefs.GetInt("AchievementUNYIELDING") = 1 &&
-        PlayerPrefs.GetInt("AchievementSTILLHERE") = 1 &&
-        PlayerPrefs.GetInt("AchievementTHECLASSICS") = 1 &&
-        PlayerPrefs.GetInt("AchievementSPEEDDEMON") = 1 &&
-        PlayerPrefs.GetInt("AchievementNORULES") = 1)
+        if (PlayerPrefs.GetInt("AchievementGAMEOVER") == 1 &&
+        PlayerPrefs.GetInt("AchievementTEAMTEN") == 1 &&
+        PlayerPrefs.GetInt("AchievementPLAYERCENTURY") == 1 &&
+        PlayerPrefs.GetInt("AchievementGRANDMASTER") == 1 && 
+        PlayerPrefs.GetInt("AchievementPARTICIPANT") == 1 && 
+        PlayerPrefs.GetInt("AchievementPERSISTENT") == 1 &&
+        PlayerPrefs.GetInt("AchievementUNYIELDING") == 1 &&
+        PlayerPrefs.GetInt("AchievementSTILLHERE") == 1 &&
+        PlayerPrefs.GetInt("AchievementTHECLASSICS") == 1 &&
+        PlayerPrefs.GetInt("AchievementSPEEDDEMON") == 1 &&
+        PlayerPrefs.GetInt("AchievementNORULES") == 1)
         {
-            PlayerPrefs.SetInt("AchievementACHIEVEMENTHUNTER") = 1;
+			PlayerPrefs.SetInt("AchievementACHIEVEMENTHUNTER", 1);
         }
         else
         {
-            PlayerPrefs.SetInt("AchievementACHIEVEMENTHUNTER") = 0;
+			PlayerPrefs.SetInt("AchievementACHIEVEMENTHUNTER", 0);
         }
     }
 }
