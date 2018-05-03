db3525b1b6aa13e345bf290da3ae6308f394ae4e
UraniumSniffer <38114445+UraniumSniffer@users.noreply.github.com>
2018-05-03 13:30:58 -0400

    Add files via upload

---
+++ GlideTeamUnity/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+  
+    public AudioSource musicSource;
+
+
+  
+    public static MusicManager extend = null;/// For other scripts to call AudioManager
+    
+    private void Awake() ///instance check
+    {
+        if (extend == null)
+            extend = this;
+        else if (extend != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+   
+        public void StartInGameMusic(AudioClip GameMusic, params AudioClip [] BGM)	///Persistent Music
+		{
+        int randomIndex = Random.Range(0, BGM.Length);
+            musicSource.clip = GameMusic;
+            musicSource.clip = BGM[randomIndex];
+            musicSource.Play();
+        }
+        public void Mute()
+        {
+            musicSource.Stop();
+        }
+      
+        public void AdjustMusic(float value)
+        {
+            float temp = value + musicSource.volume;
+            if (temp < 0 || temp > 1)
+                return;
+            else
+                musicSource.volume += value;
+        }
+    }
---
+++ GlideTeamUnity/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public AudioSource soundSource;
+
+    public AudioClip dropSound; /// Specific audio clips played when requirements are met.
+    public AudioClip LineClearing;
+    public AudioClip GameMusic;
+    public AudioClip WinTheme;
+    public AudioClip LoseTheme;
+    public AudioClip Interaction;
+    public AudioClip SceneShift;
+    public AudioClip SetBlock;
+
+
+    public void StartWinTheme() ///Specific audio clips
+    {
+
+        soundSource.clip = WinTheme;
+        soundSource.Play();
+    }
+    public void StartLoseTheme()
+    {
+
+        soundSource.clip = LoseTheme;
+        soundSource.Play();
+    }
+    public void StartInteractionSound()
+    {
+        soundSource.clip = Interaction;
+        soundSource.Play();
+    }
+    public void StartLineClearingSound()
+    {
+        soundSource.clip = LineClearing;
+        soundSource.Play();
+    }
+    public void StartdropSound()
+    {
+        soundSource.clip = dropSound;
+        soundSource.Play();
+    }
+    public void StartSceneShift()
+    {
+        soundSource.clip = SceneShift;
+        soundSource.Play();
+    }
+    public void StartBlockSet()
+    {
+        soundSource.clip = SetBlock;
+        soundSource.Play();
+    }
+    public void AdjustSoundFX(float value)  ///Volume Adjustment methods
+    {
+        float temp = value + soundSource.volume;
+        if (temp < 0 || temp > 1)
+            return;
+        else
+            soundSource.volume += value;
+    }
+}
+  
+
+
+
