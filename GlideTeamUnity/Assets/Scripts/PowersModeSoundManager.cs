8e54891ddbe3e5c266b64144cc2abfdeee2775cf
UraniumSniffer <38114445+UraniumSniffer@users.noreply.github.com>
2018-05-03 15:36:52 -0400

    Add files via upload

---
+++ GlideTeamUnity/Assets/Scripts/PowersModeSoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowersModeSoundManager : SoundManager  {
+
+
+    
+    public AudioClip DropBombBlock;
+    public AudioClip ScoreMultiplier;
+    public AudioClip WildBlockStart;
+
+     ///Specific audio clips
+  
+    public void StartDropBombBlock()
+    {
+        soundSource.clip = DropBombBlock;
+        soundSource.Play();
+
+    }
+    public void StartScoreMultiply()
+    {
+        soundSource.clip = ScoreMultiplier;
+        soundSource.Play();
+    }
+    public void StartWildBlock()
+    {
+        soundSource.clip = WildBlockStart;
+        soundSource.Play();
+
+    }
+}
---
+++ GlideTeamUnity/Assets/Scripts/TimeAttackSoundManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeAttackSoundManager : SoundManager {
+
+    public AudioClip TimerOut;
+
+    public void StartCountDownTimer()
+    {
+        soundSource.clip = TimerOut;
+        soundSource.Play();
+
+    }
+
+}
