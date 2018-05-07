6781cbdf3c1986df7f663f1709fcc716a1d6f05e
morgan738 <morgan.hu738@gmail.com>
2018-05-06 22:15:45 -0400

    time attack

---
+++ GlideTeamUnity/Assets/Scripts/Timer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : GridManager {
+
+
+	public Text timeText;
+	public float timeStamp;
+	public bool usingTimer;
+	float timeLeft = 100.0f;
+
+
+	public float seconds, minutes;
+
+
+	// Use this for initialization
+	void Start () {
+		//setTimer (30);
+
+		timeText = GetComponent<Text>() as Text;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		timeLeft -= Time.deltaTime;
+		timeText.text = timeLeft.ToString ("00");
+
+
+		if(timeLeft <= 0)
+		{
+			//game over
+
+
+
+		}
+
+		if (fullRows.Count != 0 || fullColumns.Count != 0) {
+
+			int extraTime = fullRows.Count + fullColumns.Count;
+
+			timeLeft += extraTime * 15.0f;
+
+		}
+	}
+		
+
+}
